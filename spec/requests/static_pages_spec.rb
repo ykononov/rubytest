de9ce3131eac55d16267c961d2c4a553c54ccd41
ykononov <frozen-fire@mail.ru>
2017-02-03 00:23:50 +0300

    contact page added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -27,7 +27,6 @@ describe "Static pages" do
   end
   
   describe "About page" do
-
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
@@ -36,6 +35,18 @@ describe "Static pages" do
 	it "should have the right title" do
 	  visit '/static_pages/about'
 	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+	end
+  end
+  
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+	
+	it "should have the right title" do
+	  visit '/static_pages/contact'
+	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
 	end
   end
 end
